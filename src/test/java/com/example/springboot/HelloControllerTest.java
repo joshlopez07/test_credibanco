93e1891ce576b016cb9eeae75cf83725652681f3
Joseph López <jlopez@cleveritgroup.com>
2023-12-06 22:28:57 -0500

    hellocontrollertest

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+//@AutoConfigureMockMvc
 public class HelloControllerTest {
 
 	@Autowired
