1db32b2d8f85999b1e7389c50eb631c0531e683b
Joseph López <jlopez@cleveritgroup.com>
2023-12-06 22:39:24 -0500

    hellocontroller

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,22 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	/*@GetMapping("/")
+	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
-	}*/
-	@GetMapping("/")
-        public String welcome(Model model) {
-            // Obtener la fecha y hora actuales
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-            String formattedDateTime = now.format(formatter);
-
-            // Agregar datos al modelo
-            model.addAttribute("message", "¡Bienvenido!");
-            model.addAttribute("currentDateTime", formattedDateTime);
-
-            // Devolver el nombre de la plantilla (template)
-            return "welcome";
-        }
+	}
 }
